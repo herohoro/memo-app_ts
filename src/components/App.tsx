d06b310d10d86a344ce4fa161fa2ff3e646692e9
へろほろ <mtwd.office.25@gmail.com>
2022-03-29 00:32:38 +0900

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,47 @@
+//FCはcomponentの型定義
+import { ChangeEvent, FC, useCallback, useState } from "react";
+import styled from "styled-components";
+import { MemoList } from "./MemoList";
+export const App: FC = () => {
+  //テキストボックスstate____<string>は型定義
+  const [text, setText] = useState<string>("");
+  //メモ一覧state
+  const [memos, setMemos] = useState<string[]>([]);
+
+  //テキストボックス入力時に入力内容をStateに設定
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value);
+
+  //追加ボタン押下時
+  const onClickAdd = () => {
+    //State変更を正常に検知させるため新たな配列を生成
+    const newMemos = [...memos];
+    //テキストボックスの入力内容をメモ配列に追加
+    newMemos.push(text);
+    setMemos(newMemos);
+    //テキストボックスを空に
+    setText("");
+  };
+  //削除ボタン押下時(何番目が押されたかを引数で受け取る)
+  const onClickDelete = useCallback(
+    (index: number) => {
+      //State変更を正常に検知させるため新たな配列を生成
+      const newMemos = [...memos];
+      //メモ配列から該当の要素を削除
+      newMemos.splice(index, 1);
+      setMemos(newMemos);
+    },
+    [memos]
+  );
+  return (
+    <div>
+      <h1>Hello CodeSandbox</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
+      <MemoList memos={memos} onClickDelete={onClickDelete} />
+    </div>
+  );
+};
+const SButton = styled.button`
+  margin-left: 16px;
+`;
